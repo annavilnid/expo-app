6d01b2593cf0008281c860c5af6a30aa1a5abcae
annavilnid <anna.vilnid@gmail.com>
2023-12-27 21:33:03 +0100

    add test

--- App.tsx
+++ App.tsx
@@ -56,6 +56,7 @@ export default function App() {
               <View style={styles.todolistForm}>
                 <AddItemForm placeholder={"create todolist"} buttonHandler={onPressAddTodolist}/>
               </View>
+                <p>hghggh</p>
               <View style={globalStyles.todolistList}>
                 {todolists.map(todolist => (
                     <View key={todolist.id}>
