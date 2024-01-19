02421579081e78bf219a2c888d6d4e1ee70f3069
annavilnid <anna.vilnid@gmail.com>
2024-01-19 20:25:30 +0100

    delete test

--- App.tsx
+++ App.tsx
@@ -54,7 +54,6 @@ export default function App() {
           <View style={[globalStyles.border, styles.appContainer]}>
               <SafeAreaView>
               <View style={styles.todolistForm}>
-                <p>test</p>
                 <AddItemForm placeholder={"create todolist"} buttonHandler={onPressAddTodolist}/>
               </View>
               <View style={globalStyles.todolistList}>
