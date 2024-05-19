a06ff70228a17631c473e03acfac06c14ed534b4
annavilnid <anna.vilnid@gmail.com>
2024-05-18 21:57:08 -0300

    test

--- App.tsx
+++ App.tsx
@@ -52,7 +52,6 @@ export default function App() {
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} style={[globalStyles.border, styles.appContainer]}>
           <View style={[globalStyles.border, styles.appContainer]}>
               <SafeAreaView>
-                <>t</>
               <View style={styles.todolistForm}>
                 <AddItemForm placeholder={"create todolist"} buttonHandler={onPressAddTodolist}/>
               </View>
